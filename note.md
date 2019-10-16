a25b527d84c450a936308a30a34917dbd85b4626
jackneer <clk500@hotmail.com>
2019-10-16 21:16:40 +0800

    update

---
+++ note.md
@@ -0,0 +1,5 @@
+## notes
+
+# Theme fix
+- find base.html
+- find '|format(' and replace it with '.format(slug=' without quotes 
--- pelicanconf.py
+++ pelicanconf.py
@@ -26,9 +26,17 @@ LINKS = (('Pelican', 'http://getpelican.com/'),
 
 # Social widget
 SOCIAL = (('twitter', 'https://twitter.com/jackneer'),
-	('github', 'https://githib.com/jackneer'))
+	('github', 'https://githib.com/jackneer'),
+	('envelope','mailto:clk500@hotmail.com'),)
 
 DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+THEME = '/home/jack/projects/pelican-clean-blog'
+
+# STATIC_PATHS = ['images', 'extra/favicon.ico']
+# EXTRA_PATH_METADATA = {
+#     'extra/favicon.ico': {'path': 'favicon.ico'}
+# }
